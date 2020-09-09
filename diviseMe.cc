b9a6cdc0b211365e15f8637e88bc5414b4458a31
Reedie8651 <70625373+Reedie8651@users.noreply.github.com>
2020-09-09 02:19:46 -0700

    Update diviseMe.cc

--- diviseMe.cc
+++ diviseMe.cc
@@ -4,16 +4,16 @@
 int main(void) {
 
   int num;
+  std::string s = "The divisors of "+std::to_string(num)+" are ";
+  std::cout << "Gimme a number!";
+  std::cin >> num;
+
   if(num<1){
     std::cout << "Bad number!";
   }
   if(num==1){
     std::cout << "The divisor of 1 is 1, you atonal apple.";
   }
-
-  std::string s = "The divisors of "+std::to_string(num)+" are ";
-  std::cout << "Gimme a number!";
-  std::cin >> num;
 
   for(int i=1; i<num;i++){
     if(num%i==0){
